9eb24a8088589a22618faf0dff287696bc9caa24
Barkhyunwoo <112528784+Barkhyunwoo@users.noreply.github.com>
2026-03-31 02:11:02 +0900

    건물 추가 및 적 추가
    빌딩 추가 후 할머니 배달아저씨 추가

---
+++ kimchiRun/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Spawer : MonoBehaviour
+{
+    [Header("Settings")]
+    public float minSpawnDelay;
+    public float maxSpawnDelay;
+
+    [Header("Referenece")]
+    public GameObject[] gameObjects;
+
+    
+
+    void Start()
+    {
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
+
+    }
+
+   void Spawn()
+    {
+        var randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
+        Instantiate(randomObject, transform.position, Quaternion.identity);
+        Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
+    }
+}
