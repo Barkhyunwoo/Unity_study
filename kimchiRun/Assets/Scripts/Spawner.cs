6724bd917628989c9ca3844c6ef2612159d8ece1
Barkhyunwoo <112528784+Barkhyunwoo@users.noreply.github.com>
2026-05-06 18:11:06 +0900

    스크롤 속도 점진적 증가 및 무적 색 변경
    스크롤 속도가 게임 플레이 시 점점 빨라지게 수정, 무적 색 노랑색으로 변경

--- kimchiRun/Assets/Scripts/Spawner.cs
+++ kimchiRun/Assets/Scripts/Spawner.cs
@@ -6,6 +6,9 @@ public class Spawer : MonoBehaviour
     public float minSpawnDelay;
     public float maxSpawnDelay;
 
+    public float speedIncreaseRate = 0.001f;
+
+
     [Header("Referenece")]
     public GameObject[] gameObjects;
 
@@ -27,5 +30,24 @@ public class Spawer : MonoBehaviour
         var randomObject = gameObjects[Random.Range(0, gameObjects.Length)];
         Instantiate(randomObject, transform.position, Quaternion.identity);
         Invoke("Spawn", Random.Range(minSpawnDelay, maxSpawnDelay));
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance != null && GameManager.Instance.State == GameState.Playing)
+        {
+            // 태그가 "Enemy"인 스포너일 때만 딜레이를 감소시킵니다.
+            if (gameObject.CompareTag("enemy") || gameObject.CompareTag("building"))
+            {
+                // Time.deltaTime을 곱해 매 초마다 speedIncreaseRate만큼 일정하게 줄어들도록 합니다.
+                if(minSpawnDelay > 0.5f){
+                    minSpawnDelay -= speedIncreaseRate * Time.deltaTime;
+                    maxSpawnDelay -= speedIncreaseRate * Time.deltaTime;
+                }
+                else if(maxSpawnDelay > 1f){
+                    maxSpawnDelay -= speedIncreaseRate * Time.deltaTime;
+                }
+            }
+        }
     }
 }
